9e6fa0c953245219075c0359a8931eeb8872fe81
Vaibhav Sajnani <36933274+Vaibhav1197@users.noreply.github.com>
2018-03-01 01:00:32 +0530

    Add files via upload

---
+++ sketch_feb20a/sketch_feb20a.ino
@@ -0,0 +1,34 @@
+#include <Blynk.h>
+#define BLYNK_PRINT SwSerial
+
+
+#include <SoftwareSerial.h>
+SoftwareSerial SwSerial(10, 11); // RX, TX
+    
+#include <BlynkSimpleStream.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "54aa90f6bc64476fa27616a34d85fffc";
+
+
+void setup()
+{
+  // Debug console
+  SwSerial.begin(9600);
+
+  // Blynk will work through Serial
+  // Do not read or write this serial manually in your sketch
+  Serial.begin(9600);
+  Blynk.begin(Serial, auth);
+}
+
+void loop()
+{
+  Blynk.run();
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
+}
+
+
